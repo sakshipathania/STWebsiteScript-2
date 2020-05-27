db88b34091b37465edbebff8db5250806725d1cf
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-05-27 11:50:39 +0530

    Update pdp_Facebook_Login_Paid_User_15.java

--- src/test/java/SetupClass/TestStep/pdp_Facebook_Login_Paid_User_15.java
+++ src/test/java/SetupClass/TestStep/pdp_Facebook_Login_Paid_User_15.java
@@ -20,10 +20,6 @@ public class pdp_Facebook_Login_Paid_User_15 extends Set {
 	    
 		driver.get("https://www.slideteam.net/adm-risk-monitoring-and-management-techniques-2018.html");
 		Thread.sleep(2000);
-	}
-
-	@Then("^User click on Download button to download the product xv$")
-	public void user_click_on_Download_button_to_download_the_product_xv() throws Throwable {
 		try {
 			WebElement iframe = driver.findElement(By.id("livechat-full-view"));
 			if(iframe.isDisplayed()) {
@@ -50,7 +46,12 @@ public class pdp_Facebook_Login_Paid_User_15 extends Set {
 					
 				}
 		Thread.sleep(1000);
-		WebElement download_btn_pdp_fp = wait.until(ExpectedConditions.elementToBeClickable(By.id("clicking")));
+	}
+
+	@Then("^User click on Download button to download the product xv$")
+	public void user_click_on_Download_button_to_download_the_product_xv() throws Throwable {
+		
+		WebElement download_btn_pdp_fp = driver.findElement(By.id("clicking"));
 		Thread.sleep(2000);
 		download_btn_pdp_fp.click();
 		Thread.sleep(2000);
@@ -94,33 +95,8 @@ public class pdp_Facebook_Login_Paid_User_15 extends Set {
 	@Then("^user download the product xv$")
 	public void user_download_the_product_xv() throws Throwable {
 	    Thread.sleep(1000);
-               try {
-			WebElement iframe = driver.findElement(By.id("livechat-full-view"));
-			if(iframe.isDisplayed()) {
-				driver.switchTo().frame(iframe);   
-				 Actions act = new Actions(driver);
-				 act.moveToElement(driver.findElement(By.cssSelector("#title .icon-minimize"))).build().perform();
-				 Thread.sleep(2000);
-					WebElement chat1=driver.findElement(By.cssSelector("#title .icon-minimize"));
-					 Thread.sleep(1000);
-						chat1.click();
-						 Thread.sleep(1000);
-						 driver.switchTo().defaultContent();
-						 Thread.sleep(1000);
-						 driver.switchTo().parentFrame();
-					 Thread.sleep(1000);
-			}
-			else {
-				
-
-			System.out.println("chat window does not open");
-			}
-		}
-				catch(NoSuchElementException NCP) {
-					
-				}
-		Thread.sleep(1000);
-		WebElement download_btn_pdp_fp = wait.until(ExpectedConditions.elementToBeClickable(By.id("clicking")));
+              
+		WebElement download_btn_pdp_fp = driver.findElement(By.id("clicking"));
 		Thread.sleep(2000);
 		download_btn_pdp_fp.click();
 		Thread.sleep(2000);
