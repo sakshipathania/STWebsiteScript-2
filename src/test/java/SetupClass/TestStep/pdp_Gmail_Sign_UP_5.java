ea54eff858bd8e34278949abb0d84a5800385aca
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-05-27 11:41:58 +0530

    Update pdp_Gmail_Sign_UP_5.java

--- src/test/java/SetupClass/TestStep/pdp_Gmail_Sign_UP_5.java
+++ src/test/java/SetupClass/TestStep/pdp_Gmail_Sign_UP_5.java
@@ -20,12 +20,7 @@ public class pdp_Gmail_Sign_UP_5 extends Set{
 	    
 		driver.get("https://www.slideteam.net/bar-graphs-and-pie-charts-sale-business-powerpoint-templates-themes.html");
 		Thread.sleep(2000);
-		 
-	}
-
-	@Then("^User click on Download button to download the product v$")
-	public void user_click_on_Download_button_to_download_the_product_v() throws Throwable {
-	    try {
+	try {
 			WebElement iframe = driver.findElement(By.id("livechat-full-view"));
 			if(iframe.isDisplayed()) {
 				driver.switchTo().frame(iframe);   
@@ -50,8 +45,13 @@ public class pdp_Gmail_Sign_UP_5 extends Set{
 				catch(NoSuchElementException NCP) {
 					
 				}
-		Thread.sleep(1000);
-		WebElement download_btn_pdp_fp = wait.until(ExpectedConditions.elementToBeClickable(By.id("clicking")));
+		Thread.sleep(1000);	 
+	}
+
+	@Then("^User click on Download button to download the product v$")
+	public void user_click_on_Download_button_to_download_the_product_v() throws Throwable {
+	    
+		WebElement download_btn_pdp_fp = wait.until(ExpectedConditions.elementToBeClickabledriver.findElement(By.id("clicking"));
 		Thread.sleep(2000);
 		download_btn_pdp_fp.click();
 		Thread.sleep(2000);
@@ -126,32 +126,7 @@ public class pdp_Gmail_Sign_UP_5 extends Set{
 		
 		driver.get("https://www.slideteam.net/prod-development-and-program-management.html");
 		Thread.sleep(1000);
-		try {
-			WebElement iframe = driver.findElement(By.id("livechat-full-view"));
-			if(iframe.isDisplayed()) {
-				driver.switchTo().frame(iframe);   
-				 Actions act = new Actions(driver);
-				 act.moveToElement(driver.findElement(By.cssSelector("#title .icon-minimize"))).build().perform();
-				 Thread.sleep(2000);
-					WebElement chat1=driver.findElement(By.cssSelector("#title .icon-minimize"));
-					 Thread.sleep(1000);
-						chat1.click();
-						 Thread.sleep(1000);
-						 driver.switchTo().defaultContent();
-						 Thread.sleep(1000);
-						 driver.switchTo().parentFrame();
-					 Thread.sleep(1000);
-			}
-			else {
-				
-
-			System.out.println("chat window does not open");
-			}
-		}
-				catch(NoSuchElementException NCP) {
-					
-				}
-		Thread.sleep(1000);
+		
 		driver.findElement(By.id("clicking")).click();
 		Thread.sleep(5000);
 		driver.get("https://www.slideteam.net/");
