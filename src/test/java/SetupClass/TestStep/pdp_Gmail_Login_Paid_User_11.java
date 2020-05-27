8ce2dbef2f1350844c4a0f2c4d8a3e55e3647a74
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-05-27 15:30:07 +0530

    Update pdp_Gmail_Login_Paid_User_11.java

--- src/test/java/SetupClass/TestStep/pdp_Gmail_Login_Paid_User_11.java
+++ src/test/java/SetupClass/TestStep/pdp_Gmail_Login_Paid_User_11.java
@@ -92,19 +92,19 @@ public class pdp_Gmail_Login_Paid_User_11 extends Set {
 			
 		 
 		Thread.sleep(4500);
-		WebElement gmail_email = wait.until(ExpectedConditions.elementToBeClickable(By.id("identifierId")));
+		WebElement gmail_email = driver.findElement(By.id("identifierId"));
 		Thread.sleep(2000);
 	    gmail_email.sendKeys("sakshi.pathania@slidetech.in");
 	    Thread.sleep(2000);
-	    WebElement next_1 = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#identifierNext > span:nth-child(3) > span:nth-child(1)")));
+	    WebElement next_1 = driver.findElement(By.cssSelector("#identifierNext > span:nth-child(3) > span:nth-child(1)"));
 	    Thread.sleep(2000);
 	    next_1.click();
 	    Thread.sleep(2000);
-	    WebElement gmail_pass = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".I0VJ4d > div:nth-child(1) > input:nth-child(1)")));
+	    WebElement gmail_pass = driver.findElement(By.cssSelector(".I0VJ4d > div:nth-child(1) > input:nth-child(1)"));
 	    Thread.sleep(2000);
 	    gmail_pass.sendKeys("Qwerty@1");
 	    Thread.sleep(2000);
-	    WebElement next_2 = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#passwordNext > span:nth-child(3)")));
+	    WebElement next_2 = driver.findElement(By.cssSelector("#passwordNext > span:nth-child(3)"));
 	    Thread.sleep(2000);
 	    next_2.click();
 	    Thread.sleep(2000);
