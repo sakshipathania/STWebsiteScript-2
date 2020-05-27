9063d06466baa986919a70fe3793f927d7abab32
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-05-27 14:23:43 +0530

    Update pdp_Email_Login_Correct_Data_Paid_User_8.java

--- src/test/java/SetupClass/TestStep/pdp_Email_Login_Correct_Data_Paid_User_8.java
+++ src/test/java/SetupClass/TestStep/pdp_Email_Login_Correct_Data_Paid_User_8.java
@@ -101,11 +101,7 @@ public class pdp_Email_Login_Correct_Data_Paid_User_8 extends Set {
 
 	@Then("^user logout from website viii$")
 	public void user_logout_from_website_viii() throws Throwable {
-	  Thread.sleep(3000);
-	   
-	 driver.findElement(By.xpath("//a[contains(.,'Sign Out')]")).click();
-	 Thread.sleep(3000);
-		/*Thread.sleep(1000);
+	  Thread.sleep(1000);
 		try {
 			WebElement logout = driver.findElement(By.cssSelector("//a[contains(text(),'Sign Out')]"));
 			if (logout.isEnabled()) {
@@ -116,7 +112,7 @@ public class pdp_Email_Login_Correct_Data_Paid_User_8 extends Set {
 			}
 		} catch (NoSuchElementException Ext) {
 
-		}*/
+		}
 		
 	}
 }
